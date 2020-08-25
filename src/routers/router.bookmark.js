43f4ade07d55c88291cf8e985b7cd8ec5e58828b
shiva kannojia <shivakannojia201@gmail.com>
2020-08-26 00:21:10 +0530

    Fixing summary

--- src/routers/router.bookmark.js
+++ src/routers/router.bookmark.js
@@ -1,48 +1,69 @@
-const express = require('express')
-//Requiring or importing all the model objects  from Bookmark model class 
-const Bookmark = require('../models/bookmark')
-const app = express()
-const router=new express.Router()
+const express = require("express");
+//Requiring or importing all the model objects  from Bookmark model class
+const Bookmark = require("../models/bookmark");
+const { response } = require("express");
+const app = express();
+const router = new express.Router();
 
 //Route for the creation of the Bookmark Data
-router.post('/bookmarks', (req, res) => {
-    const bookmark = new Bookmark(req.body)
+router.post("/bookmarks", (req, res) => {
+  const bookmark = new Bookmark(req.body);
 
-    bookmark.save().then(() => {
-        res.send(bookmark)
-    }).catch((e) => {
-        res.status(400).send(e)
+  bookmark
+    .save()
+    .then(() => {
+      res.send(bookmark);
     })
-})
-
-
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
 
 //Reading all the list of Bookmark
-router.get('/bookmarks', (req, res) => {
-    Bookmark.find({}).then((bookmark) => {
-        if (!bookmark) {
-            res.send('No Bookmark found')
+router.get("/bookmarks", (req, res) => {
+  Bookmark.find({})
+    .then((bookmark) => {
+      if (!bookmark) {
+        res.send("No Bookmark found");
+      }
+
+      let count = 0;
+      var groups = [];
+      bookmark.map((item) => {
+        Bgroup = item.BookmarksConfig.content.Bookmarkgroups.Bookmarkgroup;
+        count += Bgroup.length;
+        if (groups.indexOf(Bgroup[0]["name"]) < 0) {
+          groups.push(Bgroup[0]["name"]);
         }
-        res.send(bookmark)
-    }).catch((error) => {
-        res.status(500).send()
+      });
+
+      let result = {
+        TotalBookmarks: count,
+        Totalgroups: groups.length,
+        summary: `${count} bookmarks in ${groups.length} found`,
+      };
+
+      res.send(result);
     })
-})
 
-
-
+    .catch((error) => {
+      res.status(500).send();
+    });
+});
 
 //Delete a Bookmark
-router.delete('/bookmark/:id', (req, res) => {
-    const _id = req.params.id
-    Bookmark.findByIdAndDelete(_id).then((bookmark) => {
-        if (!bookmark) {
-            res.status(400).send("No Bookmark with this id exists")
-        }
-        res.send("succesfully delete")
-    }).catch(() => {
-        res.send(" Delete failed")
+router.delete("/bookmark/:id", (req, res) => {
+  const _id = req.params.id;
+  Bookmark.findByIdAndDelete(_id)
+    .then((bookmark) => {
+      if (!bookmark) {
+        res.status(400).send("No Bookmark with this id exists");
+      }
+      res.send("succesfully delete");
     })
-})
+    .catch(() => {
+      res.send(" Delete failed");
+    });
+});
 
-module.exports=router
+module.exports = router;
